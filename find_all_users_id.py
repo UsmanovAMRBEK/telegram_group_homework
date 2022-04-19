ad8d762a60f4d0c44378723c0e7a366e91eaa551
unknown <princariens@gmail.com>
2022-04-19 08:33:20 +0500

    id

--- find_all_users_id.py
+++ find_all_users_id.py
@@ -1,5 +1,4 @@
-from read_data import read_data
-
+import json
 def find_all_users_id(data: dict)->list:
     """ 
     This function will find all the users in the json file and return the list of users id
@@ -9,4 +8,14 @@ def find_all_users_id(data: dict)->list:
     Returns:
         list: List containing all the users id
     """
-    return
+    users_id = []
+    for user in data["messages"]:
+        if user["type"]=="service":
+            users_id.append(user["actor_id"])
+
+    return users_id
+
+f=open("./data/result.json")
+data=json.load(f)
+print(find_all_users_id(data))
+f.close()
