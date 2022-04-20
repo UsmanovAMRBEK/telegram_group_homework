8eb9e2dcd79ef6d6fc12ddb47434f75844e228ec
unknown <princariens@gmail.com>
2022-04-20 08:21:46 +0500

    count_mess

--- find_user_message_count.py
+++ find_user_message_count.py
@@ -10,11 +10,20 @@ def find_user_message_count(data: dict, users_id: str)->dict:
     Returns:
         dict: Number of messages of the users
     """
-    user_messages = {}
+    users_id=[]
     for user in data['messages']:
-        if user['type']=="message" and user["from_id"]==users_id:
-            user_messages[users_id] = user_messages.get(users_id, 0) + 1
+        if user['type'] == 'message' and user['from_id'] not in users_id:
+            users_id.append(user["from_id"])
+    
+    user_messages = {}
+    for user in users_id:
+        user_messages[user] = 0
+        for message in data['messages']:
+            if message['type'] == 'message' and message['from_id'] == user:
+                user_messages[user] += 1
+                
     return user_messages
+
 f=open("./data/result.json")
 data=json.load(f)
 f.close()
